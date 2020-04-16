7e0735300a5b10045908757464957e47f72af702
Artur Burecki <a.burecki@outlook.com>
2020-04-16 23:36:11 +0200

    add js validation and svg background

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector("button");
 const errorIcon = document.querySelector(".error");
 const small = document.querySelector(".small");
-
+const input = document.querySelector(".email");
 
 btn.addEventListener("click", () => {
     console.log("działa");
-    errorIcon.style.visibility = "visible";
-    small.style.visibility = "visible";
+
+    console.log(input.value);
+    if (input.value.length != 0 && input.value.includes("@")) {
+        input.value = "";
+        window.location.reload();
+    } else {
+        errorIcon.style.visibility = "visible";
+        small.style.visibility = "visible";
+    }
 })
